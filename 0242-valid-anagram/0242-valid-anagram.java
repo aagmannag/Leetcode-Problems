392354ed6c36e90d13fa5767ff7d0e043766e54d
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2024-11-13 02:37:20 +0530

    Time: 16 ms (24.1%), Space: 44.7 MB (29.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,29 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() < t.length()) return false;
+        if (s.length() != t.length()) return false;
+
         HashMap<Character, Integer> hm = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
+
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(!hm.containsKey(ch)){
-                hm.put(ch,1);
-            }
-            else{
-                hm.put(ch,hm.get(ch) + 1);
-            }
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
         }
-        for(int i = 0; i < t.length(); i++){
+
+        for (int i = 0; i < t.length(); i++) {
             char ch = t.charAt(i);
-            if(!hm.containsKey(ch)){
+
+            if (!hm.containsKey(ch)) {
                 return false;
-            }
-            else{
+            } else {
                 int count = hm.get(ch) - 1;
-                if(count > 0){
+                if (count > 0) {
                     hm.put(ch, count);
-                }
-                else{
+                } else {
                     hm.remove(ch);
                 }
-                if(hm.isEmpty()){
-                    return true;
-                }
             }
         }
-        return false;
+
+        return hm.isEmpty();
     }
 }
