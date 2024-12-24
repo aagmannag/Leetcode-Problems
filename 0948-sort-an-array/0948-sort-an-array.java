8d7799d95cb1e6fe245d23dbb06a19fef86378ff
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2024-12-24 15:57:45 +0530

    Time: 26 ms (67.84%), Space: 57.2 MB (14%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,27 +1,36 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int max = nums[0];
-        int min = nums[0];
-        for (int num : nums) {
-            if (num > max) {
-                max = num;
-            }
-            if (num < min) {
-                min = num;
-            }
-        }
-        int range = max - min + 1;
-        int[] count = new int[range];
-        for (int num : nums) {
-            count[num - min]++;
-        }
-        int index = 0;
-        for (int i = 0; i < range; i++) {
-            while (count[i] > 0) {
-                nums[index++] = i + min;
-                count[i]--;
-            }
-        }
+        mergeSort(nums, 0, nums.length-1);
         return nums;
+    }
+    private void mergeSort(int[] nums, int left, int right){
+        if(left >= right) return;
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid+1, right);
+        
+        merge(nums, left, mid, right);
+    }
+    private void merge(int[] nums, int left, int mid, int right){
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid+1, k = 0;
+
+        while(i <= mid && j <= right){
+            if(nums[i] <= nums[j]){
+                temp[k++] = nums[i++];
+            }else{
+                temp[k++] = nums[j++];
+            }
+        }
+        while(i <= mid){
+            temp[k++] = nums[i++];
+        }
+        while(j <= right){
+            temp[k++] = nums[j++];
+        }
+        
+        for(int p = 0; p < temp.length; p++){
+            nums[left + p] = temp[p];
+        }
     }
 }
