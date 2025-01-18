00985b46e9827dbe703c072024126dc3ed3f87cd
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2025-01-18 19:07:54 +0530

    Time: 0 ms (100%), Space: 40.7 MB (41.11%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        // find set Vits in num2
+        int setBits = Integer.bitCount(num2);
+        int bit = 31;
+        int res = 0;
+        while(bit >= 0 && setBits > 0){
+            // check if bit is set in num1 and if so, set bit in res
+            if((num1 & (1 << bit)) != 0){
+                res = res | (1 << bit);
+                setBits--;
+            }
+            bit--;
+        }
+        bit = 0;
+        while(setBits > 0 && bit <= 32){
+            if((num1 & (1 << bit)) == 0){
+                res = res | (1 << bit);
+                setBits--;
+            }
+            bit++;
+        }
+        return res;
+    }
+}
