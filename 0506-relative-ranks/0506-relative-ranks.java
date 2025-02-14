d63b0a9266ad87735cfe56ffbf2682c76e703a90
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2025-02-14 12:12:43 +0530

    Time: 9 ms (62.25%), Space: 45.7 MB (80.03%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,39 +1,28 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-public class Solution {
+class Solution {
     public String[] findRelativeRanks(int[] score) {
-        String[] stringArray = new String[score.length];
-        for(int i = 0; i < score.length; i++) {
-            stringArray[i] = String.valueOf(score[i]);
+        String[] StrArray = new String[score.length];
+        for(int i = 0; i < score.length; i++){
+            StrArray[i] = String.valueOf(score[i]);
         }
-        
-        // Sort the scores array in descending order
-        int[] sortedScores = Arrays.copyOf(score, score.length);
-        Arrays.sort(sortedScores);
-        
-        // Create a map to store the rank of each score
-        Map<Integer, String> rankMap = new HashMap<>();
+        int[] sorted = Arrays.copyOf(score, score.length);
+        Arrays.sort(sorted);
+        HashMap<Integer, String> map = new HashMap<>();
         int rank = 1;
-        for(int i = sortedScores.length - 1; i >= 0; i--) {
-            if (rank == 1) {
-                rankMap.put(sortedScores[i], "Gold Medal");
+        for(int i = sorted.length-1; i >= 0; i--){
+            if(rank == 1){
+                map.put(sorted[i], "Gold Medal");
             } else if (rank == 2) {
-                rankMap.put(sortedScores[i], "Silver Medal");
+                map.put(sorted[i], "Silver Medal");
             } else if (rank == 3) {
-                rankMap.put(sortedScores[i], "Bronze Medal");
+                map.put(sorted[i], "Bronze Medal");
             } else {
-                rankMap.put(sortedScores[i], String.valueOf(rank));
+                map.put(sorted[i], String.valueOf(rank));
             }
             rank++;
         }
-        
-        // Assign ranks to the original scores
         for(int i = 0; i < score.length; i++){
-            stringArray[i] = rankMap.get(score[i]);
+            StrArray[i] = map.get(score[i]);
         }
-        
-        return stringArray;
+        return StrArray;
     }
 }
