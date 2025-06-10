9bca37a6035a0b034d9ac375222173ba084d42de
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2025-06-10 12:52:31 +0530

    Time: 7 ms (48.25%), Space: 45.8 MB (28.75%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,46 +1,33 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-
-        for(int i = 0; i < numCourses; i++){
-            adj.add(new ArrayList<>());
+    public boolean canFinish(int n, int[][] prerequisites) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adjList.add(new ArrayList<>());
+        }
+        int[] indegree = new int[n];
+        for(int i = 0; i < prerequisites.length; i++){
+            adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            indegree[prerequisites[i][1]]++;
         }
 
-        for(int[] d : prerequisites){
-            adj.get(d[1]).add(d[0]);
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0) q.add(i);
         }
+        
+        List<Integer> linearOrder = new ArrayList<>();
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            linearOrder.add(curr);
 
-        boolean[] visited = new boolean[numCourses];
-        boolean[] isCurrPath = new boolean[numCourses];
-
-        for(int i = 0; i < numCourses; i++){
-            if(!visited[i] && hasCycle(adj, i, visited, isCurrPath))
-            {
-                return false;
+            for(int nbr: adjList.get(curr)){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0) q.add(nbr);
             }
         }
-        return true;
-    }
 
-    boolean hasCycle(List<List<Integer>> adj, int i, boolean[] visited, boolean[] isCurrPath){
-        if(isCurrPath[i]){
-            return true;
-        }
+        if(linearOrder.size() == n) return true;
 
-        if(visited[i]){
-            return false;
-        }
-
-        isCurrPath[i] = true;
-        visited[i] = true;
-
-        for(int nbr : adj.get(i))
-        {
-            if(hasCycle(adj, nbr, visited, isCurrPath)){
-                return true;
-            }
-        }
-        isCurrPath[i] = false;
         return false;
     }
 }
