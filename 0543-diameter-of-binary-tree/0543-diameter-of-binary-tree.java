a05055db00ca58720fbda8c7db07129f6b727488
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2025-06-15 23:38:41 +0530

    Time: 1 ms (28.93%), Space: 45.1 MB (21.53%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,27 +14,16 @@
  * }
  */
 class Solution {
-        public int diameterOfBinaryTree(TreeNode root) {
-            Diapair temp = dia(root);
-            return temp.d;
-        }
-
-        public Diapair dia(TreeNode root) {
-
-            if(root == null) return new Diapair();
-
-            Diapair left = dia(root.left);
-            Diapair right = dia(root.right);
-            Diapair sdp = new Diapair();
-            sdp.ht = Math.max(left.ht, right.ht) + 1;
-            int sd = left.ht + right.ht + 2;
-            sdp.d = Math.max(sd, Math.max(left.d, right.d));
-
-            return sdp;
-        }
-
-        class Diapair{
-            int ht = -1;
-            int d = 0;
-        }
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter(root);
+        return max;
     }
+    private int diameter(TreeNode root){
+        if(root == null) return 0;
+        int left = diameter(root.left);
+        int right = diameter(root.right);
+        max = Math.max(max, left+right);
+        return 1 + Math.max(left, right);
+    }
+}
