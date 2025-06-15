b837effc002d58cab555f39f9c431b56360c90aa
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2025-06-15 14:06:36 +0530

    Time: 1 ms (1.51%), Space: 41.8 MB (44.24%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,19 +13,51 @@
  *     }
  * }
  */
+
+ class Pair {
+    TreeNode node;
+    int num;
+
+    Pair(TreeNode node, int num) {
+        this.node = node;
+        this.num = num;
+    }
+}
+
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-        Stack<TreeNode> s = new Stack<>();
-        s.push(root);
+        Stack<Pair> s = new Stack<>();
+        s.push(new Pair(root, 1));
+        List<Integer> pre = new ArrayList<>();
+        List<Integer> in = new ArrayList<>();
+        List<Integer> post = new ArrayList<>();
+        if(root == null) return pre;
 
         while(!s.isEmpty()){
-            TreeNode store = s.pop();
-            ans.add(store.val);
-            if(store.right != null) s.push(store.right);
-            if(store.left != null) s.push(store.left);
+            Pair store = s.pop();
+
+            if(store.num == 1){
+                pre.add(store.node.val);
+                store.num++;
+                s.push(store);
+
+                if(store.node.left != null){
+                    s.push(new Pair(store.node.left, 1));
+                }
+            }
+            else if(store.num == 2){
+                in.add(store.node.val);
+                store.num++;
+                s.push(store);
+
+                if(store.node.right != null){
+                    s.push(new Pair(store.node.right, 1));
+                }
+            }
+            else{
+                post.add(store.node.val);
+            }
         }
-        return ans;
+        return pre;
     }
 }
