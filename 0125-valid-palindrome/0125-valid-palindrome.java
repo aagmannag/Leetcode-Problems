3cb54ca5d4dee8be070335e2e2f0898d947a90c5
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2025-06-18 11:49:58 +0530

    Time: 130 ms (6.26%), Space: 46.1 MB (7.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s == null) return false;
-        if(s.length() == 0) return false;
         s = s.toLowerCase();
-        int i = 0;
-        int j = s.length() - 1;
-        while(i<j)
-        {
-            if(! ((s.charAt(i) >= 'a' && s.charAt(i) <= 'z' ) || (s.charAt(i) >= '0' && s.charAt(i)<='9'))){
-                i++;
-                continue;
+        String ans = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+                ans += s.charAt(i);
             }
-            if(! ((s.charAt(j) >= 'a' && s.charAt(j) <= 'z' ) || (s.charAt(j) >= '0' && s.charAt(j)<='9'))){
-                j--;
-                continue;}
-            if(s.charAt(i) == s.charAt(j)){
+        }
+        int i = 0;
+        int j = ans.length()-1;
+        System.out.print(ans);
+        while(i < j){
+            if(ans.charAt(i) == (ans.charAt(j))){
                 i++;
                 j--;
-                continue;}
-            if(s.charAt(i) != s.charAt(j))
-            {
+            }else{
                 return false;
             }
         }
