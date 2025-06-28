630c3c34970b0c8d38bb224428a538e263b18bf8
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2025-06-28 12:44:17 +0530

    Time: 0 ms (100%), Space: 42 MB (39.02%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,27 +1,31 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int dp[][] = new int[m][n];
-
-        if (obstacleGrid[0][0] == 1) return 0;
-        dp[0][0] = 1;
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = (obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+        for(int i = 0; i < n; i++){
+            if(obstacleGrid[i][0] == 1){
+                break;
+            }else{
+                dp[i][0] = 1;
+            }
         }
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = (obstacleGrid[0][j] == 0 && dp[0][j - 1] == 1) ? 1 : 0;
+        for(int i = 0; i < m; i++){
+            if(obstacleGrid[0][i] == 1){
+                break;
+            }else{
+                dp[0][i] = 1;
+            }
         }
-        
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                if (obstacleGrid[i][j] == 0) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                } else {
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(obstacleGrid[i][j] == 1){
                     dp[i][j] = 0;
+                }else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
                 }
             }
         }
-        return dp[m-1][n-1];
+        return dp[n-1][m-1];
     }
 }
