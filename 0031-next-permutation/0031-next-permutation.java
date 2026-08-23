625fe38009ca0f1ff8f51a3961c178781cc34f75
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2026-08-23 20:10:44 +0530

    Time: 0 ms (100%), Space: 44.8 MB (16.14%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int store = Integer.MIN_VALUE, max = 0;
+        for(int i = n-1; i > 0; i--){
+            if(nums[i] > nums[i-1]){
+                store = i-1;
+                break;
+            }
+        }
+        if(store > -1){
+            for(int i = n-1; i > store; i--){
+                if(nums[i] > nums[store]){
+                    max = i;
+                    break;
+                }
+            }
+            int temp = nums[store];
+            nums[store] = nums[max];
+            nums[max] = temp;
+        }
+        int s = store+1, e = n-1;
+        if(store < -1){
+            s = 0;
+        }
+        while(s <= e){
+            int t = nums[s];
+            nums[s] = nums[e];
+            nums[e] = t;
+            s++;
+            e--;
+        }
+    }
+}   
