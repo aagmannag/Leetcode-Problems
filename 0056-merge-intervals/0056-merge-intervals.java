eed1712aa8ee1564001166e778fc1812074b26f2
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2026-09-07 12:08:21 +0530

    Time: 10 ms (21.33%), Space: 49.2 MB (42.64%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -3,19 +3,16 @@ class Solution {
         if(intervals.length <= 1){
             return intervals;
         }
-        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
-
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> result = new ArrayList<>();
-        int[] newInterval = intervals[0];
-        result.add(newInterval);
-
-        for(int[] interval : intervals){
-            if(interval[0] <= newInterval[1]){
-                newInterval[1] = Math.max(newInterval[1], interval[1]);
-            }
-            else{
-                newInterval = interval;
-                result.add(newInterval);
+        int[] interval = intervals[0];
+        result.add(interval);
+        for(int[] i : intervals){
+            if(i[0] <= interval[1]){
+                interval[1] = Math.max(interval[1], i[1]);
+            }else{
+                result.add(i);
+                interval = i;
             }
         }
         return result.toArray(new int[result.size()][]);
